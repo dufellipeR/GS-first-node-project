51861c807ac35f053adcecaf16c46bbec3cfedda
dufellipeR <eduardo.fellipe30@gmail.com>
2020-11-17 20:13:26 -0300

    TypeORM repository

--- src/database/migrations/1605649685336-CreateAppointments.ts
+++ src/database/migrations/1605649685336-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1605649685336
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
