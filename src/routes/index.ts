5aa7a41b3ffe09f68489091e1b8d9d780f8d50a3
dufellipeR <eduardo.fellipe30@gmail.com>
2020-11-19 17:25:24 -0300

    Validating Credentials

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import userRouter from './users.routes';
+import sessionsRouter from './sessions.routes';
 import appointmentsRouter from './appointments.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
-routes.use('/users', userRouter);
+routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const authenticateUser = new AuthenticateUserService();
+
+    const { user } = await authenticateUser.execute({ email, password });
+
+    const userWithoutPass = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    return res.json(userWithoutPass);
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+});
+
+export default sessionsRouter;
