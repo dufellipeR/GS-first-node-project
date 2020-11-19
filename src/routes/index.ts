97486825b0258a6f31810bb616329a8e6be09c69
dufellipeR <eduardo.fellipe30@gmail.com>
2020-11-19 17:03:40 -0300

    Building register

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import userRouter from './users.routes';
 import appointmentsRouter from './appointments.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
+routes.use('/users', userRouter);
 
 export default routes;
