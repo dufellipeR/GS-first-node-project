cfcde203a1875a8717d397118b86632263e52ab5
dufellipeR <eduardo.fellipe30@gmail.com>
2020-12-06 11:20:02 -0300

    saving tokens db

---
+++ src/modules/users/infra/http/controllers/ForgotPasswordController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
+import SendForgotPasswordEmailService from '@modules/users/services/SendForgotPasswordEmailService';
+
+export default class ForgotPasswordController {
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { email, password } = req.body;
+
+    const sendForgotPasswordEmail = container.resolve(SendForgotPasswordEmailService);
+
+    await sendForgotPasswordEmail.execute(email);
+
+    return res.status(204).json();
+  }
+}
---
+++ src/modules/users/infra/http/controllers/ResetPasswordController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
+import ResetPasswordService from '@modules/users/services/ResetPasswordService';
+
+export default class ResetPasswordController {
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { password, token} = req.body;
+
+    const resetPassword = container.resolve(ResetPasswordService);
+
+    await resetPassword.execute({
+      token,
+      password,
+    })
+
+
+    return res.status(204).json();
+  }
+}
