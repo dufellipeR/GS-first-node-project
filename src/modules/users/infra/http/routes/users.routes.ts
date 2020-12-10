3e937d82a49a1a60d3fdba477dbf46baa6a4e997
dufellipeR <eduardo.fellipe30@gmail.com>
2020-12-10 17:49:59 -0300

    Files Upload on S3 Amazon Service

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -10,7 +10,7 @@ import UsersController from '../controllers/UsersController';
 import UserAvatarController from '../controllers/UserAvatarController';
 
 const usersRouter = Router();
-const upload = multer(uploadConfig);
+const upload = multer(uploadConfig.config.disk);
 const usersController = new UsersController();
 const userAvatarController = new UserAvatarController();
 
