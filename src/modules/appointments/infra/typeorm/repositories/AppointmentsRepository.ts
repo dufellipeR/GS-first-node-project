fb0dec337093548de12ab4d34c666cf25d70eb05
dufellipeR <eduardo.fellipe30@gmail.com>
2020-12-16 10:56:25 -0300

    Addin eager loading to appointments

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -30,6 +30,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
             `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
+      relations: ['user'],
     });
 
     return appointments;
